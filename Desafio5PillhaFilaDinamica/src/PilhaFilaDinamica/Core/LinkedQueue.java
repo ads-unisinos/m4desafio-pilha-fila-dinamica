e3cb481115c2430f1c416bd8851a6c52a4e671da
beboantonio <beboantonio1@hotmail.com>
2021-06-13 16:59:23 -0300

    add LinkedQueue class

---
+++ Desafio5PillhaFilaDinamica/src/PilhaFilaDinamica/Core/LinkedQueue.java
@@ -0,0 +1,39 @@
+package PilhaFilaDinamica.Core;
+
+public class LinkedQueue<E> implements List<E>
+{
+    @Override
+    public int numElements() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+
+    @Override
+    public void insert(E element, int pos) {
+
+    }
+
+    @Override
+    public E remove(int pos) {
+        return null;
+    }
+
+    @Override
+    public E get(int pos) {
+        return null;
+    }
+
+    @Override
+    public int search(E element) {
+        return 0;
+    }
+}
