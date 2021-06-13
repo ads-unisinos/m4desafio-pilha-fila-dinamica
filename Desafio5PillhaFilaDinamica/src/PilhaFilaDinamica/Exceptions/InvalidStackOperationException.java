4f6facf8142fb730bcf99f7fc5eb384caa43b71e
beboantonio <beboantonio1@hotmail.com>
2021-06-13 16:29:35 -0300

    add InvalidStackOperationException.java

---
+++ Desafio5PillhaFilaDinamica/src/PilhaFilaDinamica/Exceptions/InvalidStackOperationException.java
@@ -0,0 +1,8 @@
+package PilhaFilaDinamica.Exceptions;
+
+public class InvalidStackOperationException extends RuntimeException
+{
+    public InvalidStackOperationException(String message) {
+        super(message);
+    }
+}
